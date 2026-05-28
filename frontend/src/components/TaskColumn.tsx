ea80147b1456b4d8165282a12320bce6e28a8137
rene <rene.ruprecht.396@gmail.com>
2026-05-28 16:19:08 +0200

    feat(frontend): add task creation via ghost card in task board

--- frontend/src/components/TaskColumn.tsx
+++ frontend/src/components/TaskColumn.tsx
@@ -1,22 +1,53 @@
+import { useState } from "react";
 import type { Task, TaskStatus } from "../types/Task";
 import TaskCard from "./TaskCard";
+import { cn } from "../utils/tailwind";
 
 interface TaskColumnProps {
   onTaskUpdate: (taskID: string, status: TaskStatus) => void;
   tasks: Task[];
+  createTask: (title: string) => void;
 }
 
-export default function TaskColumn({ onTaskUpdate, tasks }: TaskColumnProps) {
+export default function TaskColumn({
+  onTaskUpdate,
+  tasks,
+  createTask,
+}: TaskColumnProps) {
   const handleTaskUpdate = (taskID: string, status: TaskStatus) => {
     onTaskUpdate(taskID, status);
   };
+  const toggleNewCard = () => {
+    setIsCreating(!isCreating);
+  };
+  const handleCreateTask = (title: string) => {
+    createTask(title);
+    setIsCreating(false);
+  };
+
+  const [isCreating, setIsCreating] = useState(false);
   return (
     <>
       <div className="grid grid-cols-2 gap-6">
         <div>
-          <h2 className="mb-4 text-lg font-semibold">Open</h2>
+          <div className="relative flex items-center justify-center">
+            <h2 className="mb-4 px-4 text-lg font-semibold">Open</h2>
+            <button
+              className={cn("absolute right-0 mr-10 mb-4 text-lg font-bold")}
+              onClick={toggleNewCard}
+            >
+              {!isCreating ? "new" : "hide"}
+            </button>
+          </div>
 
           <div className="flex flex-col gap-3">
+            {isCreating && (
+              <TaskCard
+                mode="create"
+                onCancel={toggleNewCard}
+                onCreate={handleCreateTask}
+              />
+            )}
             {tasks
               .filter((task) => task.status == "OPEN")
               .map((task) => (
--- frontend/src/pages/TaskBoardPage.tsx
+++ frontend/src/pages/TaskBoardPage.tsx
@@ -3,16 +3,26 @@ import type { Task, Tasks, TaskStatus } from "../types/Task";
 import { getTasks } from "../api/GetTasks";
 import TaskColumn from "../components/TaskColumn";
 import { ProgressBar } from "../components/Progressbar";
+import { createTask } from "../api/CreateTask";
+import { updateTaskStatus } from "../api/UpdateTaskStatus";
 
 export default function TaskBoardPage() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const handleTaskUpdated = (taskId: string, status: TaskStatus) => {
+  const handleTaskUpdated = async (taskId: string, status: TaskStatus) => {
+    await updateTaskStatus(taskId, status);
+
     setTasks((prev) =>
       prev.map((task) => (task.id === taskId ? { ...task, status } : task)),
     );
+  };
+
+  const handleCreateTask = async (title: string) => {
+    const newTask = await createTask(title);
+    setTasks([newTask, ...tasks]);
+    console.log("handle create ", title);
   };
 
   const closedTask = tasks.filter((task) => task.status === "CLOSED");
@@ -42,12 +52,15 @@ export default function TaskBoardPage() {
 
   if (loading) return <h1>Lädt aktuelle Tasks</h1>;
   if (error) return <h1>Fehler: {error}</h1>;
-  if (tasks === undefined || tasks.length === 0) return <h1>Keine Tasks</h1>;
 
   return (
     <>
       <ProgressBar value={progress} />
-      <TaskColumn onTaskUpdate={handleTaskUpdated} tasks={tasks} />
+      <TaskColumn
+        onTaskUpdate={handleTaskUpdated}
+        tasks={tasks}
+        createTask={handleCreateTask}
+      />
     </>
   );
 }
