7700344b292b86bb6c02c6bf3a0896f412d74ca6
rene <rene.ruprecht.396@gmail.com>
2026-06-10 21:07:08 +0200

    fix(frontend): fix `taskFilter` test

--- frontend/src/features/tasks/utils/taskFilter.test.ts
+++ frontend/src/features/tasks/utils/taskFilter.test.ts
@@ -4,17 +4,17 @@ import { getClosedTasks, getOpenTasks } from "./taskFilters";
 describe("taskFilters", () => {
   it("filters open tasks", () => {
     const result = getOpenTasks([
-      { id: "1", title: "a", status: "OPEN" },
-      { id: "2", title: "b", status: "CLOSED" },
+      { id: "1", title: "a", status: "OPEN", projectID: "1" },
+      { id: "2", title: "b", status: "CLOSED", projectID: "2" },
     ]);
 
     expect(result).toHaveLength(1);
   });
   it("filters closed tasks", () => {
     const result = getClosedTasks([
-      { id: "1", title: "a", status: "OPEN" },
-      { id: "2", title: "b", status: "CLOSED" },
-      { id: "3", title: "c", status: "CLOSED" },
+      { id: "1", title: "a", status: "OPEN", projectID: "1" },
+      { id: "2", title: "b", status: "CLOSED", projectID: "2" },
+      { id: "3", title: "c", status: "CLOSED", projectID: "3" },
     ]);
 
     expect(result).toHaveLength(2);
